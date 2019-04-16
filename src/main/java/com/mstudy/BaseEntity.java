276bceeee5606bae681979d263f06229ce842be7
MKonsta <kostyan.exe@gmail.com>
2019-04-16 07:23:51 +0600

    added new class BaseEntity

---
+++ src/main/java/com/mstudy/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.mstudy;
+
+public class BaseEntity {
+    private Long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+    public boolean isNew() {
+        return (this.id == null);
+    }
+}
